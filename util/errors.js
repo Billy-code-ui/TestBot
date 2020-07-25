4a45da3052a599b2ec45fd13f3211504860975b1
Masterrr <masterpvpboi@gmail.com>
2020-07-25 13:50:50 -0400

    Update

--- util/errors.js
+++ util/errors.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const config = require('../config.json');
 
 module.exports.noPerms = (message, perm) => {
-    let embed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setTitle("Insufficient Permissions")
-    .setDescription(`Required Permission: \n ${perm}`)
+  let embed = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setTitle('Insufficient Permissions')
+    .setDescription(`Required Permission:\n${perm}`);
 
-    message.channel.send(embed)
-}
+  message.channel.send(embed);
+};
