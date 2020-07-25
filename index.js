4fc20a9e53bb28d74dd6624d16c50f137f48032a
Masterrr <masterpvpboi@gmail.com>
2020-07-25 13:21:50 -0400

    Update

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const { token } = require('./config.json');
+require('dotenv').config();
 
 const fs = require('fs');
 
-const requireAll = require('require-all'); 
+const requireAll = require('require-all');
 
-const files = requireAll({                  
-  dirname: `${__dirname}/events`,      
-  filter: /^(?!-)(.+)\.js$/              
-});                                    
+const files = requireAll({
+  dirname: `${__dirname}/events`,
+  filter: /^(?!-)(.+)\.js$/,
+});
 
-bot.removeAllListeners(); 
+bot.removeAllListeners();
 
 for (const name in files) {
   const event = files[name];
@@ -22,17 +22,16 @@ for (const name in files) {
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
+fs.readdir('./commands/', (err, files) => {
+  let jsfile = files.filter((f) => f.split('.').pop() === 'js');
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js") 
-
-    jsfile.forEach((f, i) => {
-        let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);  
-        pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name)
-        });
+  jsfile.forEach((f, i) => {
+    let pull = require(`./commands/${f}`);
+    bot.commands.set(pull.config.name, pull);
+    pull.config.aliases.forEach((alias) => {
+      bot.aliases.set(alias, pull.config.name);
     });
+  });
 });
 
-bot.login(token);
+bot.login(process.env.TOKEN);
