9227a03886f8c1c10351d32947b29d4637ddc9d1
Masterrr <masterpvpboi@gmail.com>
2020-08-24 19:14:40 -0400

    Update

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,16 +1,43 @@
 const Discord = require('discord.js');
-const fs = require('fs');
 const errors = require('../util/errors.js');
 const { color } = require('../config.json');
 const mongoose = require('mongoose');
 
 module.exports.run = async (bot, message, args) => {
-  
+  mongoose.connect(process.env.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  if (!message.member.hasPermisison("MANAGE_GUILD")) return errors.noPerms(message, "MANAGE_GUILD");
+
+  const prefixes = require('../models/prefixes.js');
+
+  let desiredPrefix = args.splice(1).join(" ");
+
+  prefixes.findOne(
+    {
+      guildID: message.guild.id
+    },
+    (err, prefix) => {
+      if (err) console.log(err);
+      if (!prefix) {
+        const newPrefix = new prefixes({
+          guildID: message.guild.id,
+          prefix: desiredPrefix,
+        });
+        newPrefix.save.save().catch((err) => console.log(err));
+      } else {
+        prefix.prefix = desiredPrefix
+      }
+      prefixes.save().catch((err) => console.log(err));
+    }
+  )
 };
 
 module.exports.config = {
-	name: 'prefix',
-	description: 'Change the prefix of the bot!',
-	usage: 'prefix',
-	aliases: ['setprefix'],
+  name: 'prefix',
+  description: 'Change the prefix of the bot!',
+  usage: 'prefix',
+  aliases: ['setprefix'],
 };
