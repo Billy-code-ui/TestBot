552062af3323c4c84a657d468c580637e763248b
Masterrr <masterpvpboi@gmail.com>
2020-09-14 19:29:01 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const { bot } = require('../index');
 
 bot.on('ready', async () => {
 	console.log(`Logged in as ${bot.user.tag}!`);
-	bot.user.setActivity('JavaScript!', { type: 'PLAYING' });
+	bot.user.setPresence({ activity: { name: 'JavaScript!', type: 'WATCHING' }, status: 'dnd' })
 });
