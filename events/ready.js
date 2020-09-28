867814eff9fd8e52a402c31bd18f5b460498165a
Masterrr <masterpvpboi@gmail.com>
2020-09-27 20:43:50 -0400

    Update

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,9 @@
 const { bot } = require('../index');
+const mongo = require('../util/mongo.js');
 
 bot.on('ready', async () => {
+	await mongo()
+
 	console.log(`Logged in as ${bot.user.tag}!`);
 	bot.user.setPresence({ activity: { name: 'JavaScript!', type: 'WATCHING' }, status: 'dnd' })
 });
